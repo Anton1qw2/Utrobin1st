da26feaccb660d9102c1b4be95326ee84b4a9db1
Anton <Anton1qw2@yandex.ru>
2017-09-17 22:57:25 +0300

    Version1.0

---
+++ Logic.py
@@ -0,0 +1,116 @@
+import cv2
+import random
+import numpy as np
+
+# create video from the picture
+def createVideo(input_img, pathVideo):
+    height, width, layers = input_img.shape
+    fours = cv2.VideoWriter_fourcc(*'MJPG')
+    frames = 15
+    video = cv2.VideoWriter(pathVideo,fours,frames,(width,height))
+    for i in range(100):
+        video.write(input_img)
+    cv2.destroyAllWindows()
+    video.release()
+
+# create video with noise from the picture
+def createVideoWithNoise(input_img, noise, pathVideo):
+    height, width, layers = input_img.shape
+    fours = cv2.VideoWriter_fourcc(*'MJPG')
+    frames = 15
+    video = cv2.VideoWriter(pathVideo, fours, frames, (width, height))
+
+    random.seed(version = 2)
+    r = random.randrange(0, 100, 1)
+    x, y, c = noise.shape
+
+    for i in range(100):
+        if i == r:
+            noise_img = input_img.copy()
+            ofs_y1 = random.randrange(0, height - x, 1)
+            ofs_x1 = random.randrange(0, width - y, 1)
+            y1, y2 = ofs_y1, ofs_y1 + noise.shape[0]
+            x1, x2 = ofs_x1, ofs_x1 + noise.shape[1]
+            alpha_s = noise[:, :, 3] / 255.0
+            alpha_l = 1.0 - alpha_s
+            for c in range(0, 3):
+                noise_img[y1:y2, x1:x2, c] = (alpha_s * noise[:, :, c] +
+                                                alpha_l * noise_img[y1:y2, x1:x2, c])
+            video.write(noise_img)
+        else:
+            video.write(input_img)
+
+# returns the frame number with an noise
+def noiseDetection(pathVideo):
+    cap = cv2.VideoCapture(pathVideo)
+    video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    for i in range(video_length):
+        _, frame = cap.read()
+        num_frame = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        frame2 = cv2.pyrDown(frame) # !
+        frame3 = cv2.pyrDown(frame2)
+        frame3 = frame3^2
+        frame1_2 = cv2.pyrUp(frame)
+        frame2_2 = cv2.pyrUp(frame2)
+        frame3_2 = cv2.pyrUp(frame3) # !
+
+        frame2_3_2 = frame2 - frame3_2 # !
+
+        gradX = cv2.Sobel(frame_gray, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=-1)
+        gradY = cv2.Sobel(frame_gray, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=-1)
+
+        gradient = cv2.subtract(gradX, gradY)
+        gradient = cv2.convertScaleAbs(gradient)
+
+        blurred = cv2.blur(gradient, (5, 5))
+        (_, thresh) = cv2.threshold(blurred, 215, 255, cv2.THRESH_BINARY)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+        closed = 255 - closed
+
+        closed = cv2.erode(closed, None, iterations=4)
+        closed = cv2.dilate(closed, None, iterations=4)
+
+        (_, cnts, _) = cv2.findContours(closed.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+
+        rect = cv2.minAreaRect(c)
+        box = cv2.boxPoints(rect)
+
+        box = np.array(box).reshape((-1, 1, 2)).astype(np.int32)
+
+        if (box[0] - box[1])[0][0] > 20 or (box[0] - box[1])[0][1] > 20:
+            cv2.drawContours(frame, [box], -1, (0, 255, 0), 3)
+            res = num_frame
+            frame_noise = frame.copy()
+
+#        cv2.imshow("Image", frame)
+#cv2.waitKey(0)
+#        cv2.waitKey(20)
+    return res, frame_noise
+'''
+# путь к текстуре
+input_img = cv2.imread('C:/Users/Natali/Documents/132.png')
+# путь к помехе
+noise = cv2.imread('C:/Users/Natali/Documents/555.png', cv2.IMREAD_UNCHANGED)
+# путь к видео (где будет создано)
+pathVideo = "C:/Users/Natali/Documents/video.avi"
+# создаем чистое видео
+createVideo(input_img, pathVideo)
+# создаем видео с помехой
+createVideoWithNoise(input_img, noise, pathVideo)
+# выводим номер кадра с найденной помехой
+res = noiseDetection(pathVideo)
+num_fr = res[0]
+print(num_fr)
+# и сам кадр
+fr = res[1]
+cv2.imshow('000', fr)
+cv2.waitKey(0)
+'''
---
+++ UI.py
@@ -0,0 +1,161 @@
+import Logic
+import cv2
+import tkinter as tk
+from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile as fo
+d= 'Stock3.jpg'
+leftvideolist= []
+def leftvideoframe(event):
+    global  leftvideolist
+    loc = leftvideolist[lslaider.get()-1]
+    loc = cvtotk(loc)
+    leftwindow.create_image(0, 0, image=loc, anchor="nw")
+    leftwindow.pack(side="left")
+    print("there")
+    print(lslaider.get())
+    root.mainloop()
+def rightvideoframe(event):
+    global rightvideolist
+    rightwindow.create_image(0, 0, image=rightvideolist[rslaider.get()], anchor="nw")
+    root.mainloop()
+def printer (event):
+    print ("hello world")
+def cvtotk(a):
+    alo = cv2.cvtColor(a, cv2.COLOR_BGR2RGB)
+    alo = Image.fromarray(alo)
+    alo = ImageTk.PhotoImage(alo)
+    return alo
+def selectimage(event):
+    global d
+    global acv
+    z = fo()
+    if z!=None:
+        d = z.name
+        print(d)
+        acv = cv2.imread(d)
+        a= cvtotk(cv2.resize(acv, (400, 400)))
+        leftwindow.create_image(0, 0, image=a, anchor="nw")
+        leftwindow.pack(side="left")
+    root.mainloop()
+def creategird(event):
+    global leftvideolist
+    Logic.createVideoWithNoise(acv, cv2.imread('out.png', cv2.IMREAD_UNCHANGED), 'myvideo1.avi')
+    video = cv2.VideoCapture('myvideo1.avi')
+    print ("done")
+    i=0
+    leftvideolist = []
+    while i<99:
+        rate, frame = video.read()
+#        frame = cv2.imread('Stock2.jpg')
+        leftvideolist.append(frame)
+        i= i+1
+    leftvideolist.append(cv2.imread('Stock2.jpg'))
+    print (len(leftvideolist))
+    global  rightvideolist
+    rightvideolist = leftvideolist
+#    global acv
+#    acv = Logic.Logic.creategrid(cv2.imread(d), wid.get(), count.get()+1, color =(blue.get()
+#                                                                                 ,green.get(),
+#                                                                                 red.get()))
+#    a = acv
+#    a = cv2.resize(a, (400, 400))
+#    a= cvtotk(a)
+#    leftwindow.create_image(0, 0, image=a, anchor="nw")
+#    leftwindow.pack(side="left")
+#    root.mainloop()
+#    print("Решетка")
+def findgird(event):
+#    cv2.imshow('234', acv)
+#    cv2.waitKey(0)
+    global acv
+    res, frame =Logic.noiseDetection('myvideo1.avi')
+    print(res)
+    cv2.imshow('noise',frame)
+    cv2.waitKey(0)
+##    a = Logic.Logic.findgrid(cv2.resize(acv, (0,0), fx=1.7, fy=1.7))
+#    a = Logic.Logic.findgrid(cv2.resize(acv, (400,400)))
+#    a = cv2.resize(a, (400, 400))
+#    a= cvtotk(a)
+#    rightwindow.create_image(0, 0, image=a, anchor="nw")
+#    rightwindow.pack(side="right")
+#    root.mainloop()
+#    print("Нашли решетку")
+
+
+root = tk.Tk()
+acv= cv2.imread(d)
+acv = cv2.resize(acv, (400,400))
+Logic.createVideo(acv, 'myvideo.avi')
+video = cv2.VideoCapture('myvideo.avi')
+i=0
+poi = [0,1,2,3,4,5,6,7,8,9]
+while i<99:
+    ret, frame=video.read()
+    leftvideolist.append(frame)
+    i= i+1
+#Настройка окон
+print(len(leftvideolist))
+rightvideolist = leftvideolist
+righttoll = tk.Frame(root,width = 50, height = 450)
+toolbar = tk.Frame(root, width=800, height = 50)
+leftimage = tk.Frame(root, width = 400, height = 500)
+rightimage = tk.Frame(root, width = 400, height = 500)
+leftwindow = tk.Canvas(leftimage, width=400, height = 400)
+rightwindow = tk.Canvas(rightimage, width = 400, height= 400)
+#Настройка кнопок
+selectim = tk.Button(toolbar, text = "Выберете текстуру")
+selectim.bind("<Button-1>", selectimage)
+selectim.pack(side = "left")
+crtgird = tk.Button(toolbar, text ="Создать помеху")
+crtgird.bind("<Button-1>", creategird )
+fndgird = tk.Button(toolbar, text = "Найти помеху")
+fndgird.bind("<Button-1>",findgird)
+fndgird.pack(side = "left")
+crtgird.pack(side = "left")
+#Текст
+tred = tk.Canvas(righttoll, width= 70, height =26)
+tred.create_text((30,15), text="Красный:")
+tgreen = tk.Canvas(righttoll, width= 70, height =26)
+tgreen.create_text((33,15), text="Зеленный:")
+tblue = tk.Canvas(righttoll, width= 70, height =26)
+tblue.create_text((22,15), text="Синий:")
+textforwidth = tk.Canvas(toolbar, width = 60, height = 40)
+textforwidth.create_text((35,10), text= "Ширина:")
+textcount = tk.Canvas(toolbar, width = 60, height = 40)
+textcount.create_text((35,10), text ="Кол-во:")
+#Настройка ползунков
+rslaider = tk.Scale(rightimage, from_=0, to= 100, length = 400, orient = tk.HORIZONTAL)
+rslaider.pack(side = "bottom")
+rslaider.bind("<ButtonRelease-3>", leftvideoframe)
+lslaider = tk.Scale(leftimage, from_=1, to= 100, length = 400, orient = tk.HORIZONTAL)
+lslaider.pack(side = "bottom")
+lslaider.bind("<ButtonRelease-3>", leftvideoframe)
+red = tk.Scale(righttoll, from_=255, to=0)
+tred.pack(side ="top")
+red.pack(side ="top")
+green = tk.Scale(righttoll, from_=255, to=0)
+tgreen.pack(side ="top")
+green.pack(side ="top")
+blue = tk.Scale(righttoll, from_=255, to=0)
+tblue.pack(side ="top")
+blue.pack(side ="top")
+wid= tk.Scale(toolbar,from_=0, to=50, orient = tk.HORIZONTAL)
+#textforwidth.pack(side="left")
+#wid.pack(side = "left")
+count = tk.Scale(toolbar, from_=0, to=20, orient = tk.HORIZONTAL)
+#textcount.pack(side = "left")
+#count.pack(side = "left")
+#настройка содержимого окон
+a = cvtotk(cv2.resize(acv, (400, 400) ))
+rightwindow.create_image(0,0, image =a, anchor = "nw")
+toolbar.pack(side = "bottom")
+#righttoll.pack(side ="right")
+rightwindow.pack(side = "bottom")
+#rightimage.pack(side = "right")
+leftwindow.create_image(0,0, image=a,anchor ="nw")
+leftwindow.pack(side = "bottom")
+leftimage.pack(side = "left")
+
+#tk.Label(root, text = "/home/anton/Downloads/345.jpg").pack()
+root.mainloop()
+
